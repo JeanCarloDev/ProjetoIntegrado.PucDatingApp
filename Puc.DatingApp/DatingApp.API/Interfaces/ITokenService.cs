30eaa9ee0af6b13d8bc4754e994b3caaab4eff95
JeanCarloDev <41553588+JeanCarloDev@users.noreply.github.com>
2022-09-05 11:03:53 -0300

    add login

---
+++ Puc.DatingApp/DatingApp.API/Interfaces/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.API.Entities;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
