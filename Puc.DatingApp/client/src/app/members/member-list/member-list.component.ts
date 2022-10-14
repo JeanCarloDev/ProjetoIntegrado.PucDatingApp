33af4f93420ca8d9bce8826cece3dde5974e0a5a
JeanCarloDev <41553588+JeanCarloDev@users.noreply.github.com>
2022-10-14 15:20:48 -0300

    update project

--- Puc.DatingApp/client/src/app/members/member-list/member-list.component.ts
+++ Puc.DatingApp/client/src/app/members/member-list/member-list.component.ts
@@ -18,7 +18,7 @@ export class MemberListComponent implements OnInit {
   pagination: Pagination;
   userParams: UserParams;
   user: User;
-  genderList = [{ value: 'male', display: 'Males' }, { value: 'female', display: 'Females' }];
+  genderList = [{ value: 'male', display: 'Masculino' }, { value: 'female', display: 'Feminino' }];
 
   constructor(private memberService: MembersService) {
     this.userParams = this.memberService.getUserParams();
